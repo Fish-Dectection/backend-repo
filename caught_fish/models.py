413b8919acb3e0c4b6fc69ce1432f761ae62ede2
DongWon <ehddnjs1747@daum.net>
2023-02-10 18:10:50 +0900

    [chore] Caught_fish 테이블 생성

--- caught_fish/models.py
+++ caught_fish/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.models import Fish
 
 # Create your models here.
+class Caught_fish(models.Model):
+    fish_length = models.FloatField()
+    user_region = models.CharField(max_length=225)
+    image_url = models.CharField(max_length=225)
+
+    main_fish_id = models.ForeignKey(Fish, on_delete=models.CASECADE)
+    main_fish_accuracy = models.FloatField()
+    sub1_fish_id = models.ForeignKey(Fish, on_delete=models.CASECADE)
+    sub1_fish_accuracy = models.FloatField()
+    sub2_fish_id = models.ForeignKey(Fish, on_delete=models.CASECADE)
+    sub2_fish_accuracy = models.FloatField()
+    
+    create_at = models.DataTimeField(default=timezone.now)
+    update_at = models.DataTimeField(default=timezone.now)
+    is_deleted = models.BooleanField(default=False)
+
+
+
+    

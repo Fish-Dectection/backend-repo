0ec5f8e8398bd53e100eb43d5b52e893e63caa3f
DongWon <ehddnjs1747@daum.net>
2023-02-20 13:23:08 +0900

    [feat] caught_fish api 수정

--- caught_fish/views.py
+++ caught_fish/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Caught_fish
-from .serializers import CaughtFishResponseSerializer, CaughtFishRequestSerializer
+from .serializers import CaughtFishSerializer
 from django.http import QueryDict
 
 
@@ -20,34 +20,30 @@ def aiResult():
 
 class Caught_fish_View(generics.CreateAPIView):
     #queryset = Caught_fish.objects.all()
-    serializer_class = CaughtFishResponseSerializer
+    serializer_class = CaughtFishSerializer
 
     def post(self, request):  
-        req_serializer =  CaughtFishResponseSerializer(data=request.data)
+        req_serializer =  CaughtFishSerializer(data=request.data)
         
         print(request)
         #요청값 데이터 베이스 저장
         req_serializer.is_valid()        
         req_serializer.save()
-                
-        #ai 결과값 받는 임시 함수
-        mfi, mfa, s1fi, s1fa, s2fi ,s2fa = aiResult()         
-        print(mfi, mfa, s1fi, s1fa, s2fi ,s2fa)
 
-        # responseVal = QueryDict('main_fish_id="{0}"&main_fish_accuracy={1}&sub1_fish_id={2}&sub1_fish_accuracy={3}&sub2_fish_id={4}&sub2_fish_accuracy={5}'.format(mfi, mfa, s1fi, s1fa, s2fi, s2fa))
-        # #responseVal = QueryDict.fromkeys(['main_fish_id','main_fish_accuracy','sub1_fish_id','sub1_fish_accuracy','sub2_fish_id','sub2_fish_accuracy'],value={'main_fish_id':mfi})
-        # print(responseVal)
+        #task_id 발급 (Task.py(셀러리)에서 구현)
+        task_id = 1
 
-        # req_serializer.is_valid()
-        
-        req_serializer = CaughtFishResponseSerializer(self, data={'main_fish_id': 'test'},partial=True)
-
-        req_serializer.is_valid()        
-        req_serializer.save()
-        
-
-        return Response({"main_fish_id": mfi, "main_fish_accuracy": mfa, "sub1_fish_id": s1fa, "sub1_fish_accuracy": s1fa, "sub2_fish_id": s2fi, "sub2_fish_accuracy": s2fa})
+        #셀러리로 전송                
+                       
+        return Response(task_id, status=200)
+       
 
 
-    def caught_create(self, serializer):
-        caught = Caught_fish.objects.get()
+    def get(self, request):
+        task_id = request.GET.get('task_id')
+        task = AsyncResult(task_id)
+        if not task.ready():
+            return Response({"WARNING"})
+        data = task.result
+        return Response(data, status=200)
+
